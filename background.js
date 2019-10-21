9a5dd959a26369ef3213f93bc7133b6fe7e8f9b7
zzzzzzzzzzzzzzz <xinggsf@gmail.com>
2019-10-21 10:11:43 +0800

    Signed-off-by: zzzzzzzzzzzzzzz <xinggsf@gmail.com>

--- background.js
+++ background.js
@@ -1,12 +1,9 @@
 const webExt = chrome || browser;
 
-const addr = new URL('https://www.test.org/');
 //用onBeforeSendHeaders更具体
 webExt.webRequest.onBeforeRequest.addListener(details => {
         const {tabId, frameId, url} = details;
-		addr.href = url;
-		const block = addr.pathname.endsWith('.m3u8') &&
-			(details.frameId != 0 ||'object' == details.type);
+		const block = details.frameId != 0 ||'object' == details.type;
         if (block) {
 			const info = { url, frameId, id: 'mv-block' };
             webExt.tabs.sendMessage(tabId, info);
@@ -14,7 +11,7 @@ webExt.webRequest.onBeforeRequest.addListener(details => {
         }
         return {cancel: block};
     },
-    { urls: ['*://*/*.m3u8*'], types: ['object', 'xmlhttprequest', 'other']},
+    { urls: ['*://*/*.m3u8','*://*/*.m3u8?*'], types: ['object', 'xmlhttprequest']},
     ["blocking"]
 );
 webExt.webRequest.onBeforeRequest.addListener(details => {
@@ -27,6 +24,6 @@ webExt.webRequest.onBeforeRequest.addListener(details => {
         }
         return {cancel: block};
     },
-    { urls: ['*://*/*'], types: ['media']},
+    { urls: ['<all_urls>'], types: ['media']},
     ["blocking"]
 );
