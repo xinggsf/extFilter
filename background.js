a1f975f83502df13110113754b0327ed53158673
xinggsf <xinggsf@gmail.com>
2020-01-15 23:57:05 +0800

    Signed-off-by: xinggsf <xinggsf@gmail.com>

--- background.js
+++ background.js
@@ -1,29 +1,22 @@
-const webExt = chrome || browser;
+const filter = details => {
+	const {tabId, frameId, url} = details;
+	const block = details.frameId != 0 ||'object' == details.type;
+	if (block) {
+		const info = { url, frameId, id: 'mv-block' };
+		chrome.tabs.sendMessage(tabId, info);
+		chrome.browserAction.enable(tabId);
+	}
+	return {cancel: block};
+};
 
 //用onBeforeSendHeaders更具体
-webExt.webRequest.onBeforeRequest.addListener(details => {
-        const {tabId, frameId, url} = details;
-		const block = details.frameId != 0 ||'object' == details.type;
-        if (block) {
-			const info = { url, frameId, id: 'mv-block' };
-            webExt.tabs.sendMessage(tabId, info);
-            webExt.browserAction.enable(tabId);
-        }
-        return {cancel: block};
-    },
+chrome.webRequest.onBeforeRequest.addListener(
+	filter,
     { urls: ['*://*/*.m3u8','*://*/*.m3u8?*'], types: ['object', 'xmlhttprequest']},
     ["blocking"]
 );
-webExt.webRequest.onBeforeRequest.addListener(details => {
-        const {tabId, frameId, url} = details;
-		const block = frameId != 0;
-        if (block) {
-			const info = { url, frameId, id: 'mv-block' };
-            webExt.tabs.sendMessage(tabId, info);
-            webExt.browserAction.enable(tabId);
-        }
-        return {cancel: block};
-    },
+chrome.webRequest.onBeforeRequest.addListener(
+	filter,
     { urls: ['<all_urls>'], types: ['media']},
     ["blocking"]
 );
--- m3u8.js
+++ m3u8.js
@@ -1,48 +1,33 @@
-const webExt = chrome || browser;
 const iframes = document.getElementsByTagName('iframe');
-const getMVFrame = () => {
-	for (let f of iframes) {
-		if (f.allowFullscreen) return f;
-	}
-};
-
 const log = console.log.bind(console);
 const q = (css, p = document) => p.querySelector(css);
-const isPlayer = (e) => {
+const isMVFlash = e => {
 	const isEmbed = e.matches('embed');
 	let s = isEmbed ? e.src : (e.data || e.children.movie.value);
 	if (!s || !/\.swf(?:$|\?)/.test(s)) return !1;
-	if (e.clientWidth > 188 && e.clientHeight > 111) return !0;
+	if (e.clientWidth > 99 && e.clientHeight > 88) return !0;
 	if (isEmbed) return !1;
 	s = q('embed', e);
-	return !!(s && isPlayer(s));
+	return !!(s && isMVFlash(s));
 };
 
-let v = null;
-webExt.runtime.onMessage.addListener((message, sender) => {
+chrome.runtime.onMessage.addListener((message, sender) => {
 	switch (message.id) {
 	case 'mv-block':
-		log(message.url);
-		if (0 != message.frameId) v = getMVFrame();
-		else {
-			for (let k of document.querySelectorAll('object,embed')) {
-				if (isPlayer(k)) {
-					log('found flash: ', k);
-					v = k;
-					break;
-				}
-			}
-		}
-		if (!v) return;
+		let v = 0 != message.frameId ?
+			[...iframes].find(e => e.allowFullscreen) :
+			[...document.querySelectorAll('object,embed')].find(isMVFlash);
+		if (!v) return;//已删除，网页重试其他地址
+		log('found MV:\n', message.url, v);
 		/*
 		const {frameId, tabId} = message;
-		webExt.webNavigation.getFrame({frameId, tabId}, function(details){
+		chrome.webNavigation.getFrame({frameId, tabId}, function(details){
 			let f = getFrame(details.url);
 			if (details.parentFrameId != -1) f = f.parent;
-		}); */
-		//const u = new URL(message.url);
-		//const t = u.pathname.endsWith('.m3u8') ? 'hls' : 'normal';
-		new DPlayer({
+		});
+		const u = new URL(message.url);
+		const t = u.pathname.endsWith('.m3u8') ? 'hls' : 'normal'; */
+		const player = new DPlayer({
 			video: {
 				url: message.url,
 				type: 'auto'
@@ -55,7 +40,7 @@ webExt.runtime.onMessage.addListener((message, sender) => {
 			switch (location.hostname) {
 			case 'www.66s.cc':
 				const el = q('.dplayer-video-wrap');
-				el.style.height = el.parentNode.clientHeight + 'px';
+				el.style.height = el.parentNode.clientHeight.toFixed(0) + 'px';
 				break;
 			case 'www.huaxingui.com':
 			case 'lefuntv.us':
