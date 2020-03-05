e11a445686697ab93666e3bb1211152734dcd40b
xinggsf <xinggsf@gmail.com>
2020-03-06 04:31:12 +0800

    dsa

--- background.js
+++ background.js
@@ -1,31 +1,37 @@
+const _noBlock = {cancel: !1};
 const filter = details => {
 	const {tabId, frameId, url, type} = details;
-	if (frameId != 0 ||'object' == type) {
+	if (tabId != -1 && (frameId != 0 ||'object' == type)) {
 		const info = { url,frameId,id: 'mv-block' };
 		chrome.tabs.sendMessage(tabId, info);
 		chrome.browserAction.enable(tabId);
 	}
-	return {cancel: !1};
+	return _noBlock;
 };
-
+/*
+const innerAddrs = [ //内部的地址白名单
+	'http://99hd.net/mp4/', // http://www.qiqidongman.com
+	'https://api.xiaomingming.org/cloud/mp4.php', // http://www.imomoe.in
+	'https://jx.123ku.com/' // https://www.cmdy5.com
+]; */
 chrome.webRequest.onBeforeRequest.addListener(
 	details => {
 		const {tabId, url} = details;
-		if (url.startsWith('https://www.yasehezi.com/')) return {cancel: !1};
-		const m = url.match(/=(http.+)$/);
-		if (m) {
-			const info = { url: m[1], frameId: 9, id: 'mv-block' };
-			setTimeout(() => {
-				chrome.tabs.sendMessage(tabId, info);
-				chrome.browserAction.enable(tabId);
-			}, 290);
+		if (tabId == -1 || url.startsWith('https://www.yasehezi.com')) return _noBlock;
+		const n = url.indexOf('=http', 15);
+		if (n > 15) {
+			const info = { 'url': url.slice(n+1), id: 'iframe-block' };
+			chrome.tabs.sendMessage(tabId, info);
+			chrome.browserAction.enable(tabId);
+            //if (innerAddrs.some(s => url.startsWith(s))) return _noBlock;
+            return {redirectUrl: 'about:blank'};
 		}
-		return {cancel: !!m};
+		return _noBlock;
 	},
     { urls: ['*://*/*.m3u8','*://*/*.mp4'], types: ['sub_frame'] },
     ["blocking"]
 );
-//用onBeforeSendHeaders更具体
+//用onBeforeSendHeaders更具体 content-type application/vnd.apple.mpegurl application/x-mpegURL video/mp2t
 chrome.webRequest.onBeforeRequest.addListener(
 	filter,
     {
--- m3u8.js
+++ m3u8.js
@@ -1,7 +1,13 @@
 const iframes = document.getElementsByTagName('iframe');
+const find = [].find.bind(iframes);
 const log = console.log.bind(console);
 const q = (css, p = document) => p.querySelector(css);
 const isPlayer = e => e.clientWidth > 221 && e.clientHeight > 111;
+const injectCSS = css => {
+	const e = document.createElement('style');
+	e.textContent = css;
+	document.head.appendChild(e);
+};
 const isMVFlash = e => {
 	const isEmbed = e.matches('embed');
 	let s = isEmbed ? e.src : (e.data || e.children.movie.value);
@@ -13,55 +19,92 @@ const isMVFlash = e => {
 };
 
 chrome.runtime.onMessage.addListener((msg, sender) => {
+	let v = null;
 	switch (msg.id) {
 	case 'mv-block':
-		let v = 0 != msg.frameId ? [...iframes].find(isPlayer) :
-			[...document.querySelectorAll('object,embed')].find(isMVFlash);
-		if (!v) return;//已删除，网页重试其他地址
-		log('found MV:\n', msg.url, v);
-		/* https://raw.githubusercontent.com/MoePlayer/DPlayer/master/dist/DPlayer.min.js
-		const {frameId, tabId} = msg;
-		chrome.webNavigation.getFrame({frameId, tabId}, function(details){
-			let f = getFrame(details.url);
-			if (details.parentFrameId != -1) f = f.parent;
-		});
-		const u = new URL(msg.url);
-		const t = u.pathname.endsWith('.m3u8') ? 'hls' : 'normal'; */
-		const dp = new DPlayer({
-			video: {
-				url: msg.url,
-				type: 'auto'
+		v = 0 != msg.frameId ? find(e => e.allowFullscreen || isPlayer(e)) :
+			[].find.call(document.querySelectorAll('object,embed'), isMVFlash);
+		break;
+	case 'iframe-block':
+		v = find(e => e.src && e.src.includes(msg.url));
+	}
+	if (!v) return;
+	log('found MV:\n', msg.url, v);
+	/* https://raw.githubusercontent.com/MoePlayer/DPlayer/master/dist/DPlayer.min.js
+	const {frameId, tabId} = msg;
+	chrome.webNavigation.getFrame({frameId, tabId}, function(details){
+		let f = getFrame(details.url);
+		if (details.parentFrameId != -1) f = f.parent;
+	});
+	const u = new URL(msg.url);
+	const t = u.pathname.endsWith('.m3u8') ? 'hls' : 'normal'; */
+	const dp = new DPlayer({
+		video: {
+			url: msg.url,
+			type: 'auto'
+		},
+		autoplay: true,
+		screenshot: true,
+		theme: '#EC8',
+		container: v.parentNode,
+		contextmenu: [
+			{
+				text: '合金H5扩展BUG反馈',
+				link: 'https://bbs.kafan.cn/thread-2162743-1-1.html'
 			},
-			autoplay: true,
-			screenshot: true,
-			container: v.parentNode
-		});
-		v.remove();
-		setTimeout(() => {
-			v = dp.video;
-			switch (location.hostname) {
-			case '5nj.com':
-			case 'www.cmdy5.com':
-			case 'kan.jinbaozy.com':
-				v.closest('.MacPlayer').style.height = 'auto';
-				dp.fullScreen.request('web');
-				break;
-			case 'www.duboku.tv':
-			case 'www.ffilmer.com':
-			case 'www.caishenys.com':
-			case 'www.i6v.cc':
-			case 'quanwo.com':
-			case 'www.61vcd.com':
-			case 'www.huaxingui.com':
-				const el = v.closest('.dplayer-video-wrap');
-				el.style.height = el.parentNode.clientHeight + 'px';
-				break;
-			case 'www.hdtt8.com':
-			case 'lefuntv.us':
-			case 'cn.inmi.tv':
-				v.closest('.fed-play-player').style.paddingTop = 0;
-			}
-		}, 330);
+		],
+	});
+	v.remove();
+	/*
+	v = dp.video;
+	switch (location.hostname) {
+	case 'www.duboku.tv':
+	case 'www.ffilmer.com':
+	case 'www.caishenys.com':
+	case 'www.xinxindm.com':
+	case 'www.i6v.cc':
+	case 'quanwo.com':
+	case 'www.61vcd.com':
+		v.closest('.dplayer-video-wrap').style.cssText = 'position:absolute!important;left:0;top:0';
+		break;
+	case 'cn.inmi.tv':
+		v.closest('.fed-play-player').style.paddingTop = 0;
+		break;
+	} */
+	dp.fullScreen.request('web');
+});
+
+setTimeout(x => {
+	switch (location.hostname) {
+	case 'www.hdtt8.com':
+	case 'cn.inmi.tv':
+		injectCSS('.dplayer{height:666px;padding-top:0!important}.dplayer-web-fullscreen-fix .fed-head-info{display:none!important} .fed-head-info{position:absolute!important;}');
+		break;
+	case 'www.dyjihe.com':
+		injectCSS('.dplayer{height:518px;}');
+		break;
+	case '5nj.com':
+	case 'www.yunbtv.com':
+	case 'www.cmdy5.com':
+	case 'kan.jinbaozy.com':
+		injectCSS('.MacPlayer{height:100%}');
+		break;
+	case 'www.hobiao.com':
+		injectCSS('.MacPlayer{height:100%} .dplayer-web-fullscreen-fix .fed-head-info{display:none!important}');
+		break;
+	case 'www.huaxingui.com':
+		injectCSS('.dplayer-web-fullscreen-fix header, .dplayer-web-fullscreen-fix #player-sidebar-is{display:none!important}');
+		break;
+	case 'www.haitur.com':
+	case 'www.haituw.com':
+	case 'www.haitum.cn':
+		injectCSS('.bottom, .dplayer-web-fullscreen-fix header{display:none!important}');
+		break;
+	case 'www.iqiyi.com':
+		injectCSS('.dplayer-web-fullscreen-fix .qy-header{display:none!important}');
+		break;
+	case 'v.qq.com':
+		injectCSS('.dplayer-web-fullscreen-fix #mod_player~*, .dplayer-web-fullscreen-fix #shortcut, .dplayer-web-fullscreen-fix .site_head{display:none!important}');
 		break;
 	}
-});
+},99);
