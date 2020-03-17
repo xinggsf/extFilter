3fc4c7410cc0ebb2cbae4de18c15a4b6eaa543e5
xinggsf <xinggsf@gmail.com>
2020-03-17 19:30:53 +0800

    dfa

--- duboku-tv.js
+++ duboku-tv.js
@@ -1,28 +1,26 @@
-const checkM3u8Url = async (url, mainAddr = '') => {
-	if (url[0] == "/") url = mainAddr + url;
-	else if (!mainAddr){
-		const u = new URL(url);
-		mainAddr = u.protocol + '//'+ u.host;
-	}
+const mainAddr = 'https://v.zdubo.com';//主视频服务器地址（前缀）
+//修正、补全为绝对地址
+const fixUrl = s => {
+	if (s[0] == "/") return  mainAddr + s;
+	return s.startsWith(mainAddr) ? s : mainAddr + s.slice(s.indexOf('/',11));
+};
+const checkM3u8Url = async (url) => {
+	url = fixUrl(url);
 	let resp = await fetch(url);
 	let txt = await resp.text();
 	const a = txt.split('\n');
-	if (a[3].trim() == '') {
-		let ret = a[2];
-		if (ret[0] == "/") ret = mainAddr + ret;
-		return ret;
-	}
+	if (a[3].trim() == '') return fixUrl(a[2]);
 	return url;
 };
 
 (async () => {
-	let v, mainAddr = 'https://v.zdubo.com';
-	v = q('.embed-responsive > script:first-of-type');
+	let src, v = q('.embed-responsive > script:first-of-type');
 	if (!v) return;
 	new MutationObserver(function(rs) {
 		v = find(e => e.matches('td>iframe'));
 		if (v) {
 			this.disconnect();
+			src = v.src;
 			v.removeAttribute('src');
 		}
 	})
@@ -31,61 +29,54 @@ const checkM3u8Url = async (url, mainAddr = '') => {
 		subtree : true
 	});
 
-	let m3u8Url, resp, addr, txt = v.textContent;
-	txt = r1(/player_data=(.+?)$/, txt);
-	const playData = JSON.parse(txt);
-	if (playData.from == 'iframe') {
-		addr = playData.url;
-		if (!addr.startsWith(mainAddr)){
-			let n = txt.indexOf('/', 11);
-			addr = mainAddr + addr.slice(n);
-		}
-		const vid = txt.split('/').pop();
-		resp = await fetch(addr);
+	try {
+		let m3u8Url, resp, vid,
+		txt = r1(/player_data=(.+?)$/, v.textContent);
+		const playData = JSON.parse(txt);
+		if (playData.from == 'iframe') {
+			//vid = txt.split('/').pop();
+			resp = await fetch(fixUrl(playData.url));
+			txt = await resp.text();
+			txt = r1(/var content\s*=\s*"(.+?)"/, txt);
+			if (!txt) {
+				alert('视频已经被移除!');
+				return;
+			}
+			const bytes = CryptoJS.AES.decrypt(txt, 'ppvod');
+			txt = bytes.toString(CryptoJS.enc.Utf8);
+			//pic = r1(/var pic = "(.+?)"/, txt);
+			//mainAddr = r1(/var redirecturl = "(.+?)"/, txt);
+			m3u8Url = r1(/var main = "(.+?)"/, txt);
+		} else m3u8Url = playData.url;
+		if (!m3u8Url) throw new Error('url error!');
+
+		m3u8Url = await checkM3u8Url(m3u8Url);
+		resp = await fetch(m3u8Url);
 		txt = await resp.text();
-		txt = r1(/var content\s*=\s*"(.+?)"/, txt);
-		if (!txt) {
-			alert('视频已经被移除!');
-			return;
+		const parser = new m3u8Parser.Parser();
+		parser.push(txt);
+		parser.end();
+		const playlist = parser.manifest.playlists;
+		while (!v) await sleep(30);
+		const playcfg = makePlayCfg(v, m3u8Url, 'hls');
+		if (playlist && playlist.length > 1) {
+			const labels = ["标清", "高清", "超清", "蓝光"];
+			const qualitys = [];
+			for (let i = 0; i < playlist.length; i++) {
+				qualitys.push({
+					name: labels[i],
+					url: fixUrl(playlist[i].uri),
+					type: 'hls'
+				});
+			}
+			playcfg.video.quality = qualitys;
+			delete playcfg.video.url;
+			playcfg.video.defaultQuality = 0;
 		}
-		const bytes = CryptoJS.AES.decrypt(txt, 'ppvod');
-		txt = bytes.toString(CryptoJS.enc.Utf8);
-		mainAddr = r1(/var redirecturl = "(.+?)"/, txt);
-		m3u8Url = r1(/var main = "(.+?)"/, txt);
-	} else m3u8Url = playData.url;
-	if (!m3u8Url) return;
-	m3u8Url = await checkM3u8Url(m3u8Url, mainAddr);
-	resp = await fetch(m3u8Url);
-	txt = await resp.text();
-	const parser = new m3u8Parser.Parser();
-	parser.push(txt);
-	parser.end();
-	const playlist = parser.manifest.playlists;
-/*
-	if (!txt || txt.endsWith('.html')) {
-		chrome.runtime.onMessage.addListener((msg, sender) => {
-			checkM3u8Url(msg.url).then(r => createPlayer(v,r,'hls'));
-		});
-		return;
-	} */
-	while (!v) await sleep(30);
-	const playcfg = makePlayCfg(v, m3u8Url, 'hls');
-	if (playlist && playlist.length > 1) {
-		const labels = ["标清", "高清", "超清", "蓝光"];
-		const qualitys = [];
-		for (let i = 0; i < playlist.length; i++) {
-			let surl = playlist[i].uri;
-			if (surl[0] == "/") surl = mainAddr + surl;
-			qualitys.push({
-				name: labels[i],
-				url: surl,
-				type: 'hls'
-			});
-		}
-		playcfg.video.quality = qualitys;
-		delete playcfg.video.url;
-		playcfg.video.defaultQuality = 0;
+		log(playcfg);
+		createPlayer(playcfg);
 	}
-	log(playcfg);
-	createPlayer(playcfg);
+	catch (ex) {
+		if (v && v.matches('iframe')) v.src = src;
+	}
 })();
--- m3u8.js
+++ m3u8.js
@@ -12,7 +12,20 @@ chrome.runtime.onMessage.addListener((msg, sender) => {
 	if (v) createPlayer(makePlayCfg(v, msg.url));
 });
 
-const cssList = {
+const router = {
+	['www.xiamov.com']() {
+		q('head > base[target="_blank"]').remove();
+	},
+	['www.yasehezi.com']() {
+		let e = document.createElement('meta');
+		e.setAttribute('name','referrer');
+		e.setAttribute('content','no-referrer');
+		document.head.appendChild(e);
+
+		e = document.createElement('script');
+		e.textContent = `setTimeout(x => { $('body').unbind('keydown'); }, 990);`;
+		document.body.appendChild(e);
+	},
 	'www.dyjihe.com': '.dplayer, #PlayContainer{height:503px;padding:0!important}',
 	'cn.funtv.cc': '.dplayer-web-fullscreen-fix .hot_banner, #fd_tips, .dplayer-web-fullscreen-fix .foot ul.extra{display:none!important}',
 	'www.haitur.com': '.bottom{display:none!important}',
@@ -20,5 +33,6 @@ const cssList = {
 	'kan.jinbaozy.com': '.dplayer:not(.dplayer-fulled) video{height:514px!important}',
 	'v.qq.com': '.dplayer-web-fullscreen-fix #mod_player~*, .dplayer-web-fullscreen-fix #shortcut, .dplayer-web-fullscreen-fix .site_head{display:none!important}'
 };
-const ss = cssList[location.hostname];
-ss && setTimeout(injectCSS,39,ss);
+const ss = router[location.hostname];
+if (typeof ss == 'string') setTimeout(injectCSS,99,ss);
+else if (typeof ss == 'function') document.addEventListener('DOMContentLoaded',ss);
