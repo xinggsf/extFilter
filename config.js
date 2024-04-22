d0136eac48d01bb4e21518f670f36c72ad0c08a7
xinggsf <xinggsf@gmail.com>
2024-04-22 23:26:40 +0800

    1.7.3 bug

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ const rawConfig = {
 	"hlsCache": false,
 	"autoWebFull": false,
 	"hostsDisableWF": [".yatu.tv","weixin.qq.com"],
-	"domainsCleanM3u": [".yatu.tv",".xyhdmw.com",".xyhdm.cc","yhdmw7.com",".mgtvys.",".qdm66.com",".856dm.com",".ddzvod.com",".nnvod.com","ikandy2.fun"],
+	"domainsCleanM3u": [".yatu.tv",".xyhdmw.",".xyhdm.","yhdmw7.com",".mgtvys.",".qdm66.com",".856dm.com",".ddzvod.com",".nnvod.com"],
 	"hostsGM": [".douban.com","dandanzan.net","nnyy.in",".btnull."]
 };
 
--- m3u8.js
+++ m3u8.js
@@ -3,11 +3,13 @@ const {r1, sleep, hookHls, dom, log, q, getPlayType} = uu;
 const router = {
 	'www.555kyy.com': '.dplayer-web-fullscreen-fix .fixedGroup, .dplayer-web-fullscreen-fix .sidebar, .dplayer-web-fullscreen-fix .header, .dplayer-web-fullscreen-fix .module-player-side{display:none!important} .dplayer:fullscreen video{height:100%!important} .MacPlayer, :not(.dplayer-fulled) video{height:600px!important}',
 	//'www.ourxh.com': '.dplayer-web-fullscreen-fix #player-sidebar-is, .dplayer-web-fullscreen-fix .myui-panel_bd{display:none!important}',
-	'acgndmku.com': '.dplayer-web-fullscreen-fix .hot_banner, #fd_tips, .dplayer-web-fullscreen-fix .foot ul.extra{display:none!important} #topnav{position: absolute!important}',
+	'acgndmku.com': 'iframe#buffer, iframe#install, .dplayer-web-fullscreen-fix .hot_banner, #fd_tips, .dplayer-web-fullscreen-fix .foot ul.extra{display:none!important} #topnav{position: absolute!important}',
+	'www.ddzvod.com':'iframe#buffer, iframe#install{display:none!important} body:not(.dplayer-web-fullscreen-fix) .MacPlayer{height:654px!important} video{max-height:100%!important}',
 	// 'ke.qq.com': '.study-video-wrapper--gray:after{display:none!important}',
 	'v.qq.com': '.dplayer-web-fullscreen-fix #mod_player~*, .dplayer-web-fullscreen-fix #shortcut, .dplayer-web-fullscreen-fix .site_head{display:none!important}',
 	'wetv.vip': '.gm-fp-body .play__aside--right, .gm-fp-body .sidebar, .gm-fp-body header{display:none!important}'
 };
+router['www.nnvod.com']= router['www.ddzvod.com'];
 const ss = router[host];
 const reLZFrame = /^https:\/\/vip\.lz-?cdn\d*\.com\/share\//;
 const iframes = document.getElementsByTagName('iframe');
@@ -195,7 +197,7 @@ chrome.runtime.onMessage.addListener((msg, sender) => {
 		cleanAds();
 	}
 	else if (cfg.hostsGM.some(k => host.includes(k))) {
-		const fn = cleanAds.toString().trim();
-		dom.script({},`-${fn}();`).remove();
+		const fnCode = cleanAds.toString().trim();
+		dom.script({},`-${fnCode}();`).remove();
 	}
 })();
