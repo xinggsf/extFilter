016fea179e4cefe4f712b15d8f0acef65cb37255
xinggsf <xinggsf@gmail.com>
2024-06-10 09:47:15 +0800

    465

--- cleanM3u.js
+++ cleanM3u.js
@@ -1,13 +1,13 @@
 //UBO m3u-prune.js https://en.wikipedia.org/wiki/M3U
 export default function() {
-	// const reFf0Lz = /\.ts\s+(#EXT-X-DISCONTINUITY).+?\1/gs;
-	const itemsHandle = [// 处理逻辑组: 神马云、量子云、非凡云
-		/^https:\/\/cdn\d*\.vipyz-cdn\d*\.com/,
-		/^https:\/\/(yzzy|hdzy|vip|tudou)\d*\.play-cdn\d*\.com/,
-		/^https:\/\/v\d*\.yzzy\d*-play\.com/,
-		/^https:\/\/(vip|hd)\d*\.lz-?cdn\d*\.com/,
-		/^https:\/\/v\d*\.cdnlz\d*\.com/,
-		/^https:\/\/(svips)?vip\.ffzy-?(?:play|online|read)\d*\./,
+	const itemsHandle = [// 处理逻辑组: 3量子云、1非凡云、3神马云
+		'lzcdn',
+		'lz-cdn',
+		'cdnlz',
+		'ffzy',
+		'vipyz-cdn',
+		'play-cdn',
+		'yzzy',
 		//暴风云 /^https:\/\/s\d*\.bfengbf\.com/ /(#EXT-X-DISCONTINUITY\n).{11,388}\1/gs
 	];
 	const urlFromArg = arg => {
@@ -15,26 +15,22 @@ export default function() {
 		if ( arg instanceof Request ) { return arg.url; }
 		return String(arg);
 	};
-	const matchM3u = url => url.endsWith('.m3u8') && itemsHandle.some(k => k.test(url));
+	const matchM3u = url => url.endsWith('.m3u8') && /\.([\w\-]+)\.com/.test(url) && itemsHandle.some(k => RegExp.$1.startsWith(k));
 	const pruner = (text) => {
 		if (/^\s*#EXTM3U/.test(text) === !1) return text;
 		const lines = text.trim().split('\n').filter(l => l !== '#EXT-X-DISCONTINUITY');
 		let i = lines.length-2;
 		if (i < 22) return lines.join('\n');
-		// const idx = lines[i].length-7;
-		// console.log('index: %i ; char: %s ; ID string: %s',idx,lines[i][idx],lines[i].slice(idx));
 		for (;lines[i].slice(-9,-7) !== '00';i-=2) {
 			lines[i] = lines[i-1] = void 0;
 		}
 		const m = lines[i].match(/(\d{4})\.ts$/);
-		// console.log('last line %i: %s', i, lines[i]);
 		if (!m) return lines.join('\n');
 
 		// const preWord = lines[i].slice(-10,-7); "a00" line[i] : adfa005123.ts
-		// const len = lines[i].length;
 		i -= 6;
 		for (const max = +m[1];i > 33;i-=2) {
-			if (lines[i].at(-8) === '0') { // && len === lines[i].length
+			if (lines[i].at(-8) === '0') {
 				const n = +lines[i].slice(-7, -3);
 				if (n < max) continue;
 			}
